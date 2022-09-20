226f5e12a21d3a5c9c07e0432d4af804c5855749
Mark simmone delos reyes <111473154+Simmoneee@users.noreply.github.com>
2022-09-20 18:17:07 +0800

    Create Gear.java

---
+++ Week 4/Gear.java
@@ -0,0 +1,17 @@
+public class Gear {
+
+String name;
+Integer Defense;   
+Float Weight;
+String Description;
+String Rarity;
+Boolean hasElementalResistance;
+
+public void showDescription(){
+System.out.println("Description" + Description);
+
+}
+
+
+
+}
