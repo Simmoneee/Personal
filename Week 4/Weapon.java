31c897d06ca48b2909e240f12ceb2500ea4443a1
Mark simmone delos reyes <111473154+Simmoneee@users.noreply.github.com>
2022-09-20 18:17:51 +0800

    Create Weapon.java

---
+++ Week 4/Weapon.java
@@ -0,0 +1,16 @@
+public class Weapon {
+   
+    int damage;
+    float weight;
+    String name; 
+    String description;
+    String rarity;
+    boolean hasElementalDamage;
+
+    
+    // Method
+    public void showDescription(){
+    System.out.println("Description" + description);
+
+}
+}
